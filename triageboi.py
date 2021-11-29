f989be51a3d57f1ad850cbf1c383b3852d26c7ae
biosboi <94704476+biosboi@users.noreply.github.com>
2021-11-29 15:55:15 -0500

    Update triageboi.py

--- triageboi.py
+++ triageboi.py
@@ -1,30 +1,42 @@
 import sys
 import hashlib
+import os
 
 def main():
     try:
         fileh = open(sys.argv[1],"rb")
-        fclass = data()
+        fclass = data(fileh)
+        fclass.printVals()
     except IndexError:
         print("Please provide input file as argument")
 
+class data():
+    def __init__(self, handle):
+        print("Class Initialized") # TEST
+        self.handle = handle
+        self.fsize = os.path.getsize(sys.argv[1])
+        self.ftype = self.getFileType()
+        #self.fhash = self.hashfile()
+    
+    def getFileType(self):
+        ftypes = {
+            b"MZ\x90" : "PE File",
+            b"ELF" : "ELF File"
+        }
+        magic = self.handle.read(3)
+        for key,value in ftypes.items():
+            if magic == key:
+                return value
+        
 
-
-class data:
-    def __init__(self):
-        self.fname = sys.argv[1]
-        #self.fsize = fsize
-        #self.ftype = ftype
-        self.fhash = self.hashfile(fname)
-
-    def hashfile(fileh):
-        buff_size = 131072# lets read stuff in 128kb chunks!
+    def hashfile(self):
+        buff_size = 65536# lets read stuff in 64kb chunks!
 
         md5 = hashlib.md5()
         sha1 = hashlib.sha1()
 
         while True:
-            data = fileh.read(buf_size)
+            data = self.read(buf_size)
             if not data:
                 break
             md5.update(data)
@@ -32,5 +44,12 @@ class data:
 
         print("MD5: {0}".format(md5.hexdigest()))
         print("SHA1: {0}".format(sha1.hexdigest()))
-    
+
+    def printVals(self):
+        print("File Name: " + sys.argv[1])
+        print("size calculated: " + str(self.fsize) + " Bytes")
+        print("File Type: " + str(self.ftype))
+        print("MD5: {0}".format(md5.hexdigest()))
+        print("SHA1: {0}".format(sha1.hexdigest()))
+
 main()
