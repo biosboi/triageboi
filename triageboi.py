ae80766b5afecf0aaec2438647c9bed775285ca4
splishy-splashy <nathankyle88@hotmail.com>
2021-12-02 18:51:49 -0500

    Added a check if the PE file was a DLL or not. Cleaned up non-standard code a little.

--- triageboi.py
+++ triageboi.py
@@ -3,14 +3,16 @@ import hashlib
 import os
 import pefile
 
+
 def main():
     try:
-        fileh = open(sys.argv[1],"rb")
+        fileh = open(sys.argv[1], "rb")
         fclass = data(fileh)
         fclass.printVals()
-        
+
     except IndexError:
         print("Please provide input file as argument")
+
 
 class data():
     def __init__(self, handle):
@@ -22,23 +24,22 @@ class data():
         if self.ftype == "PE File":
             pe = pefile.PE(sys.argv[1])
             self.pefile_info(pe)
-    
+
     def getFileType(self):
-        #Grab file type based on magic number.
-        
+        # Grab file type based on magic number.
+
         ftypes = {
-            b"MZ\x90" : "PE File",
-            b"ELF" : "ELF File"
+            b"MZ\x90": "PE File",
+            b"ELF": "ELF File"
         }
         magic = self.handle.read(3)
-        for key,value in ftypes.items():
+        for key, value in ftypes.items():
             if magic == key:
                 return value
-        
 
     def hashfile(self):
-        #Produce MD5, SHA1, and SHA256 hashse. returns as class var.
-        
+        # Produce MD5, SHA1, and SHA256 hashes. returns as class var.
+
         md5 = hashlib.md5()
         with open(sys.argv[1], 'rb') as afile:
             buf = afile.read()
@@ -53,26 +54,31 @@ class data():
         with open(sys.argv[1], 'rb') as afile:
             buf = afile.read()
             sha256.update(buf)
-            
+
         md5 = md5.hexdigest().upper()
         sha1 = sha1.hexdigest().upper()
         sha256 = sha256.hexdigest().upper()
         hashes = (md5, sha1, sha256)
-        
+
         return hashes
 
     def pefile_info(self, pe):
-        #Check if it is a 32-bit or 64-bit binary
+        # Check if it is a 32-bit or 64-bit binary
         if hex(pe.FILE_HEADER.Machine) == '0x14c':
             self.ftype = "64bit PE File"
         else:
             self.ftype = "32bit PE File"
 
-        #Compiled Time    
-        self.compile_time = ("Compiled Time: " + pe.FILE_HEADER.dump_dict()['TimeDateStamp']['Value'].split('[')[1][:-1])
-    
+        # Looking if it's a DLL or EXE
+        if pe.dump_dict()['FILE_HEADER']['Characteristics']['Value'] & 0x2000:
+            self.ftype += ' (DLL)'
+
+        # Compiled Time
+        self.compile_time = ("Compiled Time: " +
+                             pe.FILE_HEADER.dump_dict()['TimeDateStamp']['Value'].split('[')[1][:-1])
+
     def printVals(self):
-        #Produces output
+        # Produces output
         print("File Name: " + sys.argv[1])
         print("File Size: " + str(self.fsize) + " Bytes")
         print("File Type: " + str(self.ftype))
@@ -80,6 +86,6 @@ class data():
         print("MD5: " + self.fhash[0])
         print("SHA1: " + self.fhash[1])
         print("SHA256: " + self.fhash[2])
-        
+
 
 main()
