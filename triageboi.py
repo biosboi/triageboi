033d5f0ad0820581d33f05bb8100d2701081f5b3
biosboi <94704476+biosboi@users.noreply.github.com>
2021-12-09 14:05:59 -0500

    Update triageboi.py
    Imphash, pe machine type, and general cleanup

--- triageboi.py
+++ triageboi.py
@@ -12,7 +12,6 @@ def main():
         fileh = open(sys.argv[1], "rb")
         fclass = data(fileh)
         fclass.vtSearch()
-        fclass.susStrSearch()
         fclass.printVals()
         fclass.createLogFile()
 
@@ -22,11 +21,13 @@ def main():
 
 class data():
     def __init__(self, handle):
+        self.triagefile = os.path.basename(sys.argv[1])
         self.handle = handle
         self.fsize = os.path.getsize(sys.argv[1])
         self.ftype = self.getFileType()
         self.fhash = self.hashfile()
 
+        self.isDLL = False
         if self.ftype == "PE File":
             pe = pefile.PE(sys.argv[1])
             self.pefile_info(pe)
@@ -35,7 +36,7 @@ class data():
         elif self.ftype == "ELF File":
             self.elffile_info(self.handle)
         else:
-            self.isDLL = False
+            pass
 
     def getFileType(self):
         # Grab file type based on magic number.
@@ -83,6 +84,36 @@ class data():
         return hashes
 
     def pefile_info(self, pe):
+        peMachTypes = {
+            0x0    : "IMAGE_FILE_MACHINE_UNKNOWN",
+            0x1d3  : "Matsushita AM33",
+            0x8664 : "x64",
+            0x1c0  : "ARM little endian",
+            0xaa64 : "ARM64 little endian",
+            0x1c4  : "ARM Thumb-2 little endian",
+            0xebc  : "EFI byte code",
+            0x14c  : "Intel 386 or later processors and compatible processors",
+            0x200  : "Intel Itanium processor family",
+            0x6232 : "LoongArch 32-bit processor family",
+            0x6264 : "LoongArch 64-bit processor family",
+            0x9041 : "Mitsubishi M32R little endian",
+            0x266  : "MIPS16",
+            0x366  : "MIPS with FPU",
+            0x466  : "MIPS16 with FPU",
+            0x1f0  : "Power PC little endian",
+            0x1f1  : "Power PC with floating point support",
+            0x166  : "MIPS little endian",
+            0x5032 : "RISC-V 32-bit address space",
+            0x5064 : "RISC-V 64-bit address space",
+            0x5128 : "RISC-V 128-bit address space",
+            0x1a2  : "Hitachi SH3",
+            0x1a3  : "Hitachi SH3 DSP",
+            0x1a6  : "Hitachi SH4",
+            0x1a8  : "Hitachi SH5",
+            0x1c2  : "Thumb",
+            0x169  : "MIPS little-endian WCE v2"
+        }
+
         # Check if it is a 32-bit or 64-bit binary
         if hex(pe.FILE_HEADER.Machine) == '0x14c':
             self.ftype = "32bit PE File"
@@ -93,28 +124,36 @@ class data():
         if pe.dump_dict()['FILE_HEADER']['Characteristics']['Value'] & 0x2000:
             self.ftype += ' (DLL)'
             self.isDLL = True
-        else:
-            self.isDLL = False
+
+        # Machine Type
+        peMachType = pe.FILE_HEADER.Machine
+        for key,value in peMachTypes.items():
+            if peMachType == key:
+                self.peMachType = value
 
         # Compiled Time
         self.compile_time = (pe.FILE_HEADER.dump_dict()['TimeDateStamp']['Value'].split('[')[1][:-1])
-        
+
         # Imphash
-    
+        self.imphash = pe.get_imphash()
+
+        # Rich Header Hash
+        self.rHeadHash = pe.get_rich_header_hash()
+
     def dll_data(self,pe):
         # Grab Export Data
         pe.parse_data_directories(directories=[pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_EXPORT"]])
         exportData = "Ordinal: | Export Name:\n"
-        
+
         ords = [(e.ordinal) for e in pe.DIRECTORY_ENTRY_EXPORT.symbols]
         exports = [(e.name) for e in pe.DIRECTORY_ENTRY_EXPORT.symbols]
         i = 0
         while i < len(ords):
             exportData += (str(ords[i]) + "        | " + exports[i].decode("utf-8") + "\n")
             i+=1
-        
+
         return exportData
-    
+
     def elffile_info(self,handle):
         ABItypes = {
             0x0 : "System V",
@@ -126,12 +165,12 @@ class data():
             0x7 : "AIX",
             0x8 : "IRIX",
             0x9 : "FreeBSD",
-            0xA: "Tru64",
-            0xB: "Novell Modesto",
-            0xC: "OpenBSD",
-            0xD: "OpenVMS",
-            0xE: "NonStop Kernel",
-            0xF: "AROS",
+            0xA : "Tru64",
+            0xB : "Novell Modesto",
+            0xC : "OpenBSD",
+            0xD : "OpenVMS",
+            0xE : "NonStop Kernel",
+            0xF : "AROS",
             0x10: "Fenix OS",
             0x11: "CloudABI",
             0x12: "Stratus Technologies OpenVOS"
@@ -256,77 +295,83 @@ class data():
                 hVTLog = open(sys.argv[1] + "_VT_REPORT.json", "a")
                 hVTLog.write(self.VTOutput)
                 hVTLog.close()
+            hVTLog.close()
 
     def susStrSearch(self):
         pass
 
     def printVals(self):
         # Print output to console
-        print("\nStandard Data:")
-        print("File Name: " + sys.argv[1])
-        print("File Size: " + str(self.fsize) + " Bytes")
-        print("File Type: " + str(self.ftype))
-        print("MD5: " + self.fhash[0])
-        print("SHA1: " + self.fhash[1])
-        print("SHA256: " + self.fhash[2])
+        print("\nStandard Data:" + \
+               "\nFile Name: " + str(self.triagefile) + \
+               "\nFile Size: " + str(self.fsize) + " Bytes" + \
+               "\nFile Type: " + str(self.ftype) + \
+               "\nMD5: " + self.fhash[0] + \
+               "\nSHA1: " + self.fhash[1] + \
+               "\nSHA256: " + self.fhash[2] + "\n")
 
         # Conditional prints based on file type
         if "PE" in self.ftype:
-            print("\nPE Data:")
-            print("Compiled Time: " + self.compile_time)
+            print("\nPE Data:" + \
+                  "\nMachine Type: " + self.peMachType + \
+                  "\nImphash: " + self.imphash + \
+                  "\nRich Header Hash: " + self.rHeadHash + \
+                  "\nCompiled Time: " + self.compile_time)
         if self.isDLL == True:
-            print("\n\nDLL Data:")
-            print(self.exportData)
+            print("\n\nDLL Data:" + \
+                  self.exportData)
         if "ELF" in self.ftype:
-            print("\nELF Data:")
-            print("ABI: " + self.abi)
-            print("Object File Type: " + self.objtype)
-            print("Machine Type: " + self.machtype)
-            
+            print("\nELF Data:" + \
+                  "\nABI: " + self.abi + \
+                  "\nObject File Type: " + self.objtype + \
+                  "\nMachine Type: " + self.machtype + "\n")
+
         # Write VT results to Log file
         print("\nVirusTotal Data:")
         if self.VTsuccess == True:
-            print("\nScan Result: " + str(self.result['positives']) + "/" + str(self.result['total']))
-            print("\nLink to Report: " + str(self.result['permalink']))
+            print("\nScan Result: " + str(self.result['positives']) + "/" + str(self.result['total']) + \
+                  "\nLink to Report: " + str(self.result['permalink']))
         else:
-            print("\nImplement VirusTotal API Key for VirusTotal Results")
+            print("Implement VirusTotal API Key for VirusTotal Results")
 
     def createLogFile(self):
         # Write output to log file
-        hLogFile = open(sys.argv[1] + "_TRIAGE_LOG.txt", "a")
-        welcome = "TRIAGEBOI Log Output For File: " + sys.argv[1] + "\n\n" + \
-                  "TRIAGEBOI is Written and directed by [William (Nathan] Robinson)\n\n"
-        hLogFile.write(welcome)
-
-        hLogFile.write("\nStandard Data:\n")
-        hLogFile.write("File Name: " + sys.argv[1] + "\n")
-        hLogFile.write("File Size: " + str(self.fsize) + " Bytes\n")
-        hLogFile.write("File Type: " + str(self.ftype) + "\n")
-        hLogFile.write("MD5: " + self.fhash[0] + "\n")
-        hLogFile.write("SHA1: " + self.fhash[1] + "\n")
-        hLogFile.write("SHA256: " + self.fhash[2] + "\n")
+        hLogFile = open(self.triagefile + "_TRIAGE_LOG.txt", "a")
+        hLogFile.write("\n" + "-"*65 + "\nTRIAGEBOI Log Output For File: " + self.triagefile + "\n\n" + \
+                       "TRIAGEBOI is Written and directed by [William (Nathan] Robinson)" + "\n" + "-"*65 + "\n")
+        hLogFile.write("\nStandard Data:" + \
+                        "\nFile Name: " + str(self.triagefile) + \
+                        "\nFile Size: " + str(self.fsize) + " Bytes" + \
+                        "\nFile Type: " + str(self.ftype) + \
+                        "\nMD5: " + self.fhash[0] + \
+                        "\nSHA1: " + self.fhash[1] + \
+                        "\nSHA256: " + self.fhash[2] + "\n")
 
         # Conditional prints based on file type
         if "PE" in self.ftype:
-            hLogFile.write("\n\nPE Data:\n")
-            hLogFile.write("Compiled Time: " + self.compile_time + "\n")
+            hLogFile.write("\n\nPE Data:" + \
+                           "\nMachine Type: " + self.peMachType + \
+                           "\nImphash: " + self.imphash + \
+                           "\nRich Header Hash: " + self.rHeadHash + \
+                           "\nCompiled Time: " + self.compile_time + "\n")
         if self.isDLL == True:
-            hLogFile.write("\n\nDLL Data:")
-            hLogFile.write("\n" + self.exportData)
+            hLogFile.write("\n\nDLL Data:" + \
+                           "\n" + self.exportData)
         elif "ELF" in self.ftype:
-            hLogFile.write("\n\nELF Data:\n")
-            hLogFile.write("ABI: " + self.abi + "\n")
-            hLogFile.write("Object File Type: " + self.objtype + "\n")
-            hLogFile.write("Machine Type: " + self.machtype + "\n")
+            hLogFile.write("\n\nELF Data:" + \
+                           "\nABI: " + self.abi + \
+                           "\nObject File Type: " + self.objtype + \
+                           "\nMachine Type: " + self.machtype)
 
         # Write VT results to Log file
         hLogFile.write("\n\nVirusTotal Data:")
         if self.VTsuccess == True:
-            hLogFile.write("\nScan Result: " + str(self.result['positives']) + "/" + str(self.result['total']))
-            hLogFile.write("\nLink to Report: " + str(self.result['permalink']))
+            hLogFile.write("\nScan Result: " + str(self.result['positives']) + "/" + str(self.result['total']) + \
+                           "\nLink to Report: " + str(self.result['permalink']))
         else:
             hLogFile.write("\nImplement VirusTotal API Key for VirusTotal Results")
 
         hLogFile.close()
 
-main()
+if __name__ == "__main__":
+    main()
